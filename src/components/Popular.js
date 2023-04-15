994fbfa60d1b88531e02c75f24a7213c103b77c1
samchillcott1 <samchillcott@gmail.com>
2023-04-15 09:48:24 +0100

    add fetch request

--- src/components/Popular.js
+++ src/components/Popular.js
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const Popular = () => {
+  useEffect(() => {
+    getPopular();
+  }, [])
+
+
+  const getPopular = async () => {
+    const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_SPOONACULAR_API_KEY}&number=9`)
+    const data = await api.json
+    console.log(data);
+  }
   return (
     <div>Popular</div>
   )
