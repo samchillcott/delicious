fa49cb10fd9406653a3114c611e00cfe0874b339
samchillcott1 <samchillcott@gmail.com>
2023-04-15 11:18:05 +0100

    add map function

--- src/components/Popular.js
+++ src/components/Popular.js
@@ -10,10 +10,19 @@ const Popular = () => {
   const getPopular = async () => {
     const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_SPOONACULAR_API_KEY}&number=9`)
     const data = await api.json()
-    setPopular(data.recipies);
+    setPopular(data.recipes);
   }
   return (
-    <div>Popular</div>
+    <div>
+      { popular.map((recipe) => {
+        return (
+          <div key={ recipe.id }>
+            <p>{ recipe.title }</p>
+          </div>
+        )
+      })
+      }
+    </div>
   )
 }
 
