433296e3abacd31fa3c155604bf1ac7037c265c9
samchillcott1 <samchillcott@gmail.com>
2023-04-15 11:04:20 +0100

    add state for popular

--- src/components/Popular.js
+++ src/components/Popular.js
@@ -1,15 +1,16 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Popular = () => {
   useEffect(() => {
     getPopular();
   }, [])
 
+  const [popular, setPopular] = useState([])
 
   const getPopular = async () => {
     const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_SPOONACULAR_API_KEY}&number=9`)
-    const data = await api.json
-    console.log(data);
+    const data = await api.json()
+    setPopular(data.recipies);
   }
   return (
     <div>Popular</div>
