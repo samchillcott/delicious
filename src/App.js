44bc5373f86c5e52ee70e8ed8f053ddb559ea592
samchillcott1 <samchillcott@gmail.com>
2023-04-16 11:26:26 +0100

    add SpliceSlides to each mapped card

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Pages from "./pages/Pages";
 function App() {
   return (
     <div className="App">
-    <h1>Hola</h1>
+    <h1>Recipes</h1>
     <Pages />
     </div>
   );
--- src/components/Popular.js
+++ src/components/Popular.js
@@ -1,4 +1,7 @@
 import React, { useEffect, useState } from 'react'
+import styled from 'styled-components';
+import { Splide, SplideSlide } from "@splidejs/react-splide"
+import '@splidejs/react-splide/css';
 
 const Popular = () => {
   useEffect(() => {
@@ -11,19 +14,40 @@ const Popular = () => {
     const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_SPOONACULAR_API_KEY}&number=9`)
     const data = await api.json()
     setPopular(data.recipes);
+    console.log(data.recipes)
   }
   return (
     <div>
-      { popular.map((recipe) => {
-        return (
-          <div key={ recipe.id }>
-            <p>{ recipe.title }</p>
-          </div>
-        )
-      })
-      }
+      <Wrapper>
+        <h3>Popular Picks</h3>
+        <Splide>
+          { popular.map((recipe) => {
+            return (
+              <SplideSlide>
+                <Card>
+                  <p>{ recipe.title }</p>
+                  <img src={ recipe.image } alt={ recipe.title } />
+                </Card>
+              </SplideSlide>
+            )
+          }) }
+        </Splide>
+      </Wrapper>
     </div>
   )
 }
+
+const Wrapper = styled.div`
+  margin: 4rem 0rem
+`
+
+const Card = styled.div`
+  min-height: 25rem;
+  border-radius: 2rem;
+  overflow: hidden;
+  img {
+    border-radius: 2rem
+  }
+`
 
 export default Popular
