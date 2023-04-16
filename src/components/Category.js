c0c1a150640050b7e7237989b99daf4540c64f36
samchillcott1 <samchillcott@gmail.com>
2023-04-16 15:34:23 +0100

    add NavLinks to categories

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,28 +1,35 @@
 import { FaPizzaSlice, FaHamburger } from 'react-icons/fa'
 import { GiNoodles, GiChopsticks } from 'react-icons/gi'
-// import { NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
+import styled from 'styled-components';
 
 const Category = () => {
   return (
-    <div>
-      <div className="">
+    <List>
+      <NavLink to={ '/cuisine/Italian' }>
         <FaPizzaSlice />
         <h4>Italian</h4>
-      </div>
-      <div className="">
-        <FaHamburger  />
+      </NavLink>
+      <NavLink to={ '/cuisine/American' }>
+        <FaHamburger />
         <h4>American</h4>
-      </div>
-      <div className="">
+      </NavLink>
+      <NavLink to={ '/cuisine/Thai' }>
         <GiNoodles />
         <h4>Thai</h4>
-      </div>
-      <div className="">
+      </NavLink >
+      <NavLink to={ '/cuisine/Japanese' } >
         <GiChopsticks />
         <h4>Japanese</h4>
-      </div>
-    </div>
+      </ NavLink>
+    </List>
   )
 }
+
+const List = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 2rem 0rem;
+`;
 
 export default Category
