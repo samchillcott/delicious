181cba31c009b5d4403b8e64a9dd96b65baa939e
samchillcott1 <samchillcott@gmail.com>
2023-04-16 12:32:53 +0100

    add Gradient component

--- src/components/Popular.js
+++ src/components/Popular.js
@@ -35,6 +35,7 @@ const Popular = () => {
                 <Card>
                   <p>{ recipe.title }</p>
                   <img src={ recipe.image } alt={ recipe.title } />
+                  <Gradient />
                 </Card>
               </SplideSlide>
             )
@@ -78,6 +79,14 @@ const Card = styled.div`
     justify-content: center;
     align-items: center;
   }
+`;
+
+const Gradient = styled.div`
+  z-index: 3;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5))
 `;
 
 export default Popular
