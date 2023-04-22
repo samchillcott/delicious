51ecba2a0b33cdc895362b108e1fcd2f099124f2
samchillcott1 <samchillcott@gmail.com>
2023-04-22 14:14:47 +0100

    add search functionality

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,13 +1,28 @@
-import React from 'react'
-import styled from 'styled-components';
+import React, { useState } from 'react'
+
 import { FaSearch } from 'react-icons/fa'
+import { useNavigate } from 'react-router-dom';
+
+import styled from 'styled-components';
 
 const Search = () => {
+  const [input, setInput] = useState("")
+  const navigate = useNavigate()
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    navigate("/searched/" + input)
+  }
+
   return (
-    <FormStyle>
+    <FormStyle onSubmit={ submitHandler }>
       <div>
-        <FaSearch/>
-        <input type="text" />
+        <FaSearch />
+        <input
+          type="text"
+          onChange={ (e) => setInput(e.target.value) }
+          value={ input }
+        />
       </div>
     </FormStyle>
   )
