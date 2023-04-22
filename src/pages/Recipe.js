3f1081d5bf2b1e702971acb41ed6339699bc0e8b
samchillcott1 <samchillcott@gmail.com>
2023-04-22 16:29:14 +0100

    add SC and activeTab state

--- src/pages/Recipe.js
+++ src/pages/Recipe.js
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import styled from 'styled-components'
 
 const Recipe = () => {
   let params = useParams()
   const [details, setDetails] = useState({})
+  const [activeTab, setActiveTab] = useState("instructions")
+
   const fetchDetails = async () => {
     const data = await fetch(`https://api.spoonacular.com/recipes/${params.name}/information?apiKey=${process.env.REACT_APP_SPOONACULAR_API_KEY}&number=1`)
     const detailData = await data.json()
@@ -15,10 +18,53 @@ const Recipe = () => {
   }, [params.name])
 
   return (
-    <div>
-      { details.title }
-    </div>
+    <DetailedWrapper>
+      <div className="">
+        <h2>{ details.title }</h2>
+        <img src={ details.image } alt="" />
+      </div>
+      <Info >
+        <Button onClick={ () => setActiveTab("instructions") } >
+          Instructions
+        </Button>
+        <Button onClick={ () => setActiveTab("ingredients") }>
+          Ingredients
+        </Button>
+      </Info>
+    </DetailedWrapper>
   )
 }
 
+const DetailedWrapper = styled.div`
+  margin-top: 10rem;
+  margin-bottom: 5rem;
+  display: flex;
+  .active {
+    background: linear-gradient(35deg, #494949, #313131);
+    color: white;
+  }
+  h2 {
+    margin: 2rem;
+  }
+  li {
+    font-size: 1.2rem;
+    line-height: 2.5rem;
+  }
+  ul {
+    margin-top: 2rem;
+  }
+`;
+
+const Button = styled.button`
+  padding: 1rem 2rem;
+  color: #313131;
+  background: white;
+  border: 2px solid black;
+  margin-right: 2rem;
+  font-weight: 600;
+`;
+
+const Info = styled.div`
+  margin-left: 10rem;
+`;
 export default Recipe
