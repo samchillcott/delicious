bb3aecc413adf99c7fdb0b81c0030324e9b28216
samchillcott1 <samchillcott@gmail.com>
2023-04-22 17:01:40 +0100

    add nav logo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import { BrowserRouter } from "react-router-dom";
-import Category from "./components/Category";
-import Search from "./components/Search";
+import styled from "styled-components";
+import { GiKnifeFork } from "react-icons/gi";
+import { BrowserRouter, Link } from "react-router-dom";
+
 import Pages from "./pages/Pages";
+import Search from "./components/Search";
+import Category from "./components/Category";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Nav>
+          <GiKnifeFork />
+          <Logo to={ "/" }>deliciouss</Logo>
+        </Nav>
         <Search />
         <Category />
         <Pages />
@@ -14,5 +21,22 @@ function App() {
     </div>
   );
 }
+
+const Logo = styled(Link)`
+  text-decoration: none;
+  font-size: 1.5rem;
+  font-weight: 400;
+  font-family: "Lobster Two", cursive;
+`;
+
+const Nav = styled.div`
+  padding: 4rem 0;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  svg {
+    font-size: 2rem;
+  }
+`
 
 export default App;
